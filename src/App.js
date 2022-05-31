4661bb2057176a9b2876355aa80c9435526870e0
assindhu <92337472+assindhu@users.noreply.github.com>
2022-05-31 17:55:32 +0200

    changed App file

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div classNmae="WeatherApp">WeatherApp</div>;
+  return (
+    <div classNmae="WeatherApp">
+      WeatherApp
+      <footer>
+        This project was created by Sindhu{" "}
+        <a
+          href="https://github.com/assindhu/ReactWeatherApp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open sourced github
+        </a>
+      </footer>
+    </div>
+  );
 }
