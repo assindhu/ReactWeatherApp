1f8a9b77fe50aea972663e8eb5b7c3cd7a3cf670
assindhu <92337472+assindhu@users.noreply.github.com>
2022-05-31 17:41:48 +0200

    Initial commit

---
+++ build/precache-manifest.e163137e929d79db8f81345bb8d711d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df41e3d5f64dc356c33d6d5474e9a1a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c73704eebca9cde53fa3",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "bb733141a9bc2b082500",
+    "url": "/static/js/2.0e72afad.chunk.js"
+  },
+  {
+    "revision": "c73704eebca9cde53fa3",
+    "url": "/static/js/main.87ba621a.chunk.js"
+  },
+  {
+    "revision": "d50777f6420249be67bc",
+    "url": "/static/js/runtime-main.26c1a369.js"
+  }
+]);
