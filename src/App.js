68145d5858b782ce470cb47102371c4a4c6820cf
assindhu <92337472+assindhu@users.noreply.github.com>
2022-06-01 13:56:21 +0200

    added styling and HTML files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React from "react";
-
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
-    <div classNmae="WeatherApp">
-      WeatherApp
-      <footer>
-        This project was created by Sindhu{" "}
-        <a
-          href="https://github.com/assindhu/ReactWeatherApp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open sourced github
-        </a>
-      </footer>
+    <div className="App">
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was created by Sindhu{" "}
+          <a
+            href="https://github.com/assindhu/ReactWeatherApp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open sourced github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
